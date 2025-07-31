7ee2e36a82acb255e6cc67dadf9d43f346c91b79
Rohan E R Kesarkar <141849488+rohanerk@users.noreply.github.com>
2025-07-31 23:06:11 +0530

    Add files via upload

---
+++ GUI/mouse.py
@@ -0,0 +1,7 @@
+import pyautogui
+
+for i in range(10):
+    pyautogui.moveTo(1,1,duration = 1)
+    pyautogui.moveTo(1919,1,duration = 1)
+    pyautogui.moveTo(1919,1078,duration = 1)
+    pyautogui.moveTo(1,1079,duration = 1)
